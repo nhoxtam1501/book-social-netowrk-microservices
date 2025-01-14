f1ed015b7da8e6994b32662f82e0efafe65ec548
Duc Tam ( ͡• ͜ʖ ͡• ) <huynhductam69@gmail.com>
2025-01-14 15:24:32 +0700

    added tests

--- profile-service/src/main/java/com/devteria/profile/controllers/UserProfileController.java
+++ profile-service/src/main/java/com/devteria/profile/controllers/UserProfileController.java
@@ -5,6 +5,7 @@ import com.devteria.profile.dto.response.UserProfileCreationResponse;
 import com.devteria.profile.service.UserProfileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.filter.RequestContextFilter;
@@ -18,8 +19,9 @@ public class UserProfileController {
     private final RequestContextFilter requestContextFilter;
 
     @PostMapping
-    public ResponseEntity<UserProfileCreationResponse> createUserProfile(@RequestBody UserProfileCreationRequest request) {
-        return ResponseEntity.ok(service.createUserProfile(request));
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserProfileCreationResponse createUserProfile(@RequestBody UserProfileCreationRequest request) {
+        return service.createUserProfile(request);
     }
 
     @GetMapping("/{userId}")
--- profile-service/src/test/java/com/devteria/profile/ProfileServiceApplicationTests.java
+++ profile-service/src/test/java/com/devteria/profile/ProfileServiceApplicationTests.java
@@ -1,13 +1,76 @@
 package com.devteria.profile;
 
+import com.devteria.profile.controllers.UserProfileController;
+import com.devteria.profile.dto.request.UserProfileCreationRequest;
+import com.devteria.profile.entites.UserProfile;
+import com.devteria.profile.repositories.UserProfileRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.Neo4jContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@Testcontainers
 class ProfileServiceApplicationTests {
+    @Container
+    private static Neo4jContainer<?> neo4jContainer = new Neo4jContainer<>(DockerImageName.parse("neo4j:latest"))
+            .withoutAuthentication(); // Disable password
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private UserProfileRepository userProfileRepository;
+    @Autowired
+    private UserProfileController controller;
 
-	@Test
-	void contextLoads() {
-	}
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.neo4j.uri", neo4jContainer::getBoltUrl);
+    }
+
+
+    @Test
+    void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
+
+
+    @Test
+    void shouldCreateUserProfile() throws Exception {
+        //*context-path must not to specify in urlTemplate
+        mockMvc.perform(MockMvcRequestBuilders.post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(createUserProfile())))
+                .andExpect(status().isCreated());
+        assertThat(userProfileRepository.findAll().size()).isGreaterThan(0);
+        UserProfile userProfile = userProfileRepository.findAll().get(userProfileRepository.findAll().size() - 1);
+        assertThat(userProfile.getFirstName()).isEqualTo("Lena");
+    }
+
+    private UserProfileCreationRequest createUserProfile() {
+        return UserProfileCreationRequest.builder()
+                .firstName("Lena")
+                .lastName("Supa")
+                .city("New Mexico")
+                .dob(LocalDate.of(1989, 2, 13))
+                .build();
+    }
 
 }
