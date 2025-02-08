f2883282e6f9ef9344de3db57038e0dc8d2a212c
Duc Tam ( ͡• ͜ʖ ͡• ) <huynhductam69@gmail.com>
2025-02-08 13:56:55 +0700

    added public endpoint

--- api-gateway/src/main/java/com/devteria/gateway/filters/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/devteria/gateway/filters/AuthenticationFilter.java
@@ -2,18 +2,22 @@ package com.devteria.gateway.filters;
 
 import com.devteria.gateway.services.IdentityService;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.NonFinal;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -21,10 +25,19 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AuthenticationFilter implements GlobalFilter, Ordered {
     private final IdentityService identityService;
-
+    @Value("${app.api-prefix}")
+    private String apiPrefix;
+    @NonFinal
+    private String[] publicEndpoints = {
+            "/identity/auth/.*",
+            "/identity/users/registration"
+    };
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         List<String> authHeaders = exchange.getRequest().getHeaders().get((HttpHeaders.AUTHORIZATION));
+        if (isPublicEndpoint(exchange.getRequest()))
+            return chain.filter(exchange);
+
         if (CollectionUtils.isEmpty(authHeaders)) {
             return unauthenticated(exchange.getResponse());
         }
@@ -48,5 +61,10 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
         String message = "Unauthenticated";
         response.setStatusCode(HttpStatus.UNAUTHORIZED);
         return response.writeWith(Mono.just(response.bufferFactory().wrap(message.getBytes())));
+    }
+
+    private boolean isPublicEndpoint(ServerHttpRequest request){
+        return Arrays.stream(publicEndpoints)
+                .anyMatch(s -> request.getURI().getPath().matches(apiPrefix + s));
     }
 }
